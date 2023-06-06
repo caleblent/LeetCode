1e99bb0666fc192513ec9cbc3a19c3dc22f2ef70
caleblent <caleb@stcroixwebagency.com>
2023-06-05 21:16:46 -0500

    refactored

---
+++ Python/Test.py
@@ -0,0 +1,38 @@
+# comment
+
+var1 = "Python thing"
+
+list1 = [
+    "L33t",
+    "C0de"
+]
+
+var2 = list1[0]
+var3 = list1[1]
+
+print(var2)
+print(var3)
+
+list4 = [
+    'k', 'E', 'l', 'p', 'I', 'z', 'G', 'B', 'b', 'C', 'P', 'w', 'Y', '6', 'Q', '7', 'P', 'G', 'I', 'C,'
+    '2', 'w', 'D', 'V', 'u', '0', 'L', 'R', 'U', 'F', 'y', 'j', 'P', 'E', 'F', 'D', 'u', 'S', '5', '0', 'z', '0'
+]
+
+set4 = {
+    'k', 'E', 'l', 'p', 'I', 'z', 'G', 'B', 'b', 'C', 'P', 'w', 'Y', '6', 'Q', '7', 'P', 'G', 'I', 'C',
+    '2', 'w', 'D', 'V', 'u', '0', 'L', 'R', 'U', 'F', 'y', 'j', 'P', 'E', 'F', 'D', 'u', 'S', '5', '0', 'z', '0'
+}
+
+print(list4)
+print(set4)
+
+print()
+
+for thing in set4:
+    print(thing)
+
+print()
+print()
+
+for i in range(0, 10):
+    print(list4[i])
